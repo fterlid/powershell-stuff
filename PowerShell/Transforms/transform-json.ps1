419ae92ae2e5f016531b200227b80b0fd9bcb189
fterlid <fterlid@gmail.com>
2016-12-01 23:50:44 +0100

    parametere

--- PowerShell/Transforms/transform-json.ps1
+++ PowerShell/Transforms/transform-json.ps1
@@ -1,1 +1,22 @@
-﻿Write-Host "Hello world"
+﻿param(
+	[Parameter(Mandatory = $true, HelpMessage = "Angi filsti til kilde")]
+	[Alias("source")]
+	[ValidatePattern(".+\.json$")]
+	[string]
+	$sourcePath,
+
+	[Parameter(Mandatory=$true, HelpMessage="Angi miljø")]
+	[Alias("env", "miljø")]
+    [ValidateSet("DEV", "TEST", "PROD")]
+    [string]
+    $environment,
+
+    [Parameter(HelpMessage="Angi url til xxx")]
+    [ValidatePattern("^http[s]?://.*")]
+    [string]
+    $url
+)
+
+Write-Host $sourcePath
+Write-Host $environment
+Write-Host $url
